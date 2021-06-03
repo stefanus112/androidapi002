e57a57a67acdb721ce8368e2dcd6af118301b63b
stefanus agung laksono <55822156+stfn002@users.noreply.github.com>
2021-06-03 12:51:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const importData = require("./data.json");
+
+app.get("/",(req, res)=>{
+    res.send("hello word");    
+});
 
 app.get("/kelurahan",function(reg,res){
-    res.send("importData")
+    res.send(importData)
 });
 
 app.listen(process.env.PORT || 8080);
